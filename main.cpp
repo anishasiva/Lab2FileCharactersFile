3b1ae1785587d610961bf2015c04c361be00c133
Anisha Siva <asiva014@xe-02.cs.ucr.edu>
2022-10-06 21:37:39 -0700

    Fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <fstream>
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
 // Place charCnt prototype (declaration) here
+int charCnt(string numfile, char letter);
+
 
 
 int main() {
@@ -27,3 +30,37 @@ int main() {
 }
 
 // Place charCnt implementation here
+
+ int charCnt(string numfile, char letter) {
+   ifstream inputs; // inputs IS the stream
+
+   // number of times the letter shows up in the input
+
+
+// opening the file --> streamName.open();
+inputs.open(numfile);
+
+
+string line;
+
+// check if the file is even opening using the .open() function
+if (!inputs.is_open())  {
+   cout << "Error opening " << numfile << endl;
+   exit(1);
+}
+
+int counter = 0;
+
+while (getline(inputs, line)) {
+
+
+
+
+   for (int i = 0; i < line.size(); ++i)   {
+      if (line.at(i) == letter)   {
+         counter = counter + 1;
+      }
+   }
+}
+   return counter; 
+}
